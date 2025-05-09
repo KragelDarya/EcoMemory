f325851fc66191b9739d15b27b843376f4b252e8
KragelDarya <dasha12300227@gmail.com>
2025-05-10 02:57:20 +0300

    first

---
+++ Memory/Memory/LoadingForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace Memory
+{
+    partial class LoadingForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingForm));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LoadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 644);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoadingForm";
+            this.Text = "Memory";
+            this.Load += new System.EventHandler(this.LoadingForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Memory/Memory/LoadingForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Memory
+{
+    public partial class LoadingForm : Form
+    {
+        private Timer timerr;
+        private int elapsedTime = 0;
+        private string backgroundPath;
+        public LoadingForm()
+        {
+            InitializeComponent();
+            InitializeSplash();
+        }
+        private void InitializeSplash()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Width = 1000;
+            this.Height = 700;
+
+            ProgressBar progressBar = new ProgressBar();
+            progressBar.Style = ProgressBarStyle.Continuous;
+            progressBar.Width = (int)(300);
+            progressBar.Height = (int)(40); 
+            progressBar.Top = (this.ClientSize.Height - progressBar.Height) / 2;
+            progressBar.Left = (this.ClientSize.Width - progressBar.Width) / 2;
+            progressBar.Name = "progressBar";
+            this.Controls.Add(progressBar);
+
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += (s, e) =>
+            {
+                elapsedTime += 100;
+                progressBar.Value = Math.Min(progressBar.Maximum, (int)(progressBar.Maximum * elapsedTime / 3000));
+                if (elapsedTime >= 3600)
+                {
+                    timer.Stop();
+                    MainForm mainForm = new MainForm();
+                    mainForm.Show();
+                    this.Hide(); 
+                }
+            };
+            timer.Start();
+        }
+
+        private void LoadingForm_Load(object sender, EventArgs e)
+        {
+            backgroundPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "files/f2.bmp");
+            this.BackgroundImage = System.Drawing.Image.FromFile(backgroundPath);
+        }
+    }
+}
